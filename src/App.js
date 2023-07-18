cebe8ffb4cd5f5de7990cd58009cacc13808f87f
arun murali <90950678+Arun12Muralidharan@users.noreply.github.com>
2023-07-18 12:23:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import "./styles.css";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
 export default function App() {
@@ -28,33 +25,54 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [openNum, setOpenNum] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((item, index) => (
         <AccordionItem
           title={item.title}
-          text={item.text}
+          openNum={openNum}
+          setOpenNum={setOpenNum}
           num={index + 1}
           key={index}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        title={"test question 1"}
+        openNum={openNum}
+        setOpenNum={setOpenNum}
+        num={23}
+        key={22}
+      >
+        <p>
+          test answer
+          <ul>
+            <li>list item 1</li>
+            <li>list item 2</li>
+            <li>list item 3</li>
+          </ul>
+        </p>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [visible, setVisible] = useState(false);
+function AccordionItem({ num, title, openNum, setOpenNum, children }) {
+  const visible = num === openNum;
 
-  function handleHide() {
-    setVisible((visible) => !visible);
+  function handleOpen() {
+    setOpenNum(visible ? null : num);
   }
 
   return (
-    <div className={`item ${visible ? "open" : ""}`} onClick={handleHide}>
+    <div className={`item ${visible ? "open" : ""}`} onClick={handleOpen}>
       <p className="number">{num < 10 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{!visible ? `+` : `-`}</p>
-      {visible && <div className="content-box">{text}</div>}
+      {visible && <div className="content-box">{children}</div>}
     </div>
   );
 }
